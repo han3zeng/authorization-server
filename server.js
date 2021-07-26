24160e659ac7fdc9a153d25a7918214f91ab6f50
Han <allen0346@gmail.com>
2021-07-26 13:47:02 +0800

    Adjust: github auth with creating User in database

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const server = require('./src/app');
 const port = process.env.PORT;
+const db = require('./src/db');
 
-server.listen(port, () => {
-  console.log(`the server is listening on port: ${port}`);
-});
+db.connect()
+  .then(() => {
+    server.listen(port, () => {
+      console.log(`the server is listening on port: ${port}`);
+    });
+  })
+  .catch(() => {
+    console.log('Oops, fail to start the server.');
+  });
--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,16 +2,24 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 const { githubSecret } = require('../../secrets');
+const { userSchema } = require('../db/models');
+const mongoose = require('mongoose');
 
-router.post('/github', async function (req, res, next) {
-  const body = req.body;
-  const { code, clientID, redirectUri } = body;
+// return type:
+// accessToken: data.access_token,
+// tokenType: data.token_type,
+// scope: data.scope
+async function fetchAccessToken ({
+  code,
+  clientID,
+  redirectUri
+}) {
   const options = {
     method: 'POST',
     mode: 'cors',
     cache: 'no-cache',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     },
     redirect: 'follow',
@@ -23,23 +31,78 @@ router.post('/github', async function (req, res, next) {
       client_secret: githubSecret
     })
   };
-  fetch('https://github.com/login/oauth/access_token', options)
-    .then((response) => {
-      response.json()
-        .then((data) => {
-          res.json({
-            accessToken: data.access_token,
-            tokenType: data.token_type,
-            scope: data.scope
-          });
-        })
-        .catch((err) => {
-          console.log('err: ', err);
-        });
-    })
-    .catch((err) => {
-      console.log('err: ', err);
+  const response = await fetch('https://github.com/login/oauth/access_token', options);
+  const data = await response.json();
+  return data;
+}
+
+async function fetchUserProfile ({
+  accessToken
+}) {
+  const options = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'no-cache',
+    headers: {
+      Accept: 'application/json',
+      Authorization: `token ${accessToken}`
+    }
+  };
+  const response = await fetch('https://api.github.com/user', options);
+  const data = await response.json();
+  return {
+    name: data.name,
+    email: data.email,
+    avatarURL: data.avatar_url
+  };
+}
+
+async function createUser (storeData) {
+  const User = mongoose.model(userSchema.key, userSchema.schema);
+  const { email } = storeData;
+  const ifExisted = await User.exists({
+    email
+  });
+  if (!ifExisted) {
+    await User.create({
+      ...storeData
     });
+  } else {
+    await User.updateOne({
+      email
+    }, {
+      accessToken: storeData.accessToken
+    });
+  }
+}
+
+router.post('/github', async function (req, res, next) {
+  const body = req.body;
+  const { code, clientID, redirectUri } = body;
+  try {
+    let userProfile = null;
+    const data = await fetchAccessToken({
+      code,
+      clientID,
+      redirectUri
+    });
+    const accessToken = data.access_token;
+    if (accessToken) {
+      userProfile = await fetchUserProfile({
+        accessToken
+      });
+    }
+    const storeData = {
+      accessToken,
+      ...userProfile
+    };
+    await createUser(storeData);
+    res.status(200).json({
+      accessToken
+    });
+  } catch (e) {
+    console.log('e: ', e);
+  }
 });
 
 module.exports = router;
