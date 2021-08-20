b9c4454dcfbd389d158201416d361d6935ff91a6
Han <allen0346@gmail.com>
2021-08-20 21:53:37 +0800

    Adjust: get token from env

--- src/utils/mail-transporter.js
+++ src/utils/mail-transporter.js
@@ -1,18 +1,12 @@
 const nodemailer = require('nodemailer');
-const { readFileSync } = require('fs');
-const path = require('path');
-const { gmailSecret } = process.env;
+const { gmailSecret, gmailClientId, token: tokenString } = process.env;
 
-const TOKEN_PATH = path.resolve(__dirname, '../../token.json');
-const tokenBinary = readFileSync(TOKEN_PATH);
-const token = JSON.parse(tokenBinary);
-
-const clientId = '1085091594760-3osdss6996tb3f4drohm41sv59ep6tfo.apps.googleusercontent.com';
+const token = JSON.parse(tokenString);
 
 const auth = {
   type: 'OAuth2',
   user: 'han3zeng@gmail.com',
-  clientId,
+  clientId: gmailClientId,
   clientSecret: gmailSecret,
   refreshToken: token.refresh_token,
   accessToken: token.access_token,
