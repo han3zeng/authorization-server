b3b68081d1b732fadde7bb72a2101a0d5d6c6d80
Han <allen0346@gmail.com>
2021-08-23 14:58:22 +0800

    Adjust: config

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const constants = require('./constants');
 
 module.exports = Object.freeze({
   nodeEnv: process.env.NODE_ENV,
-  allowedOrigins: ['http://localhost:3000'],
-  clientOrigin: process.env.NODE_ENV === constants.production ? 'https:xxx' : 'http://localhost:3000',
+  allowedOrigins: ['http://localhost:3000', 'https://react-playground-7kgn6zbeya-uc.a.run.app'],
+  clientOrigin: process.env.NODE_ENV === constants.production ? 'https://react-playground-7kgn6zbeya-uc.a.run.app' : 'http://localhost:3000',
   clientCallbackPath: 'login-callback'
 });
